f629945f7cf098c7b1180884e6a8dcd03d9ca6fa
Ayodele Fagbami <afagbami@gmail.com>
2022-06-04 10:40:44 +0100

    Fixed Profile Screen

--- lib/screens/profile/components/body.dart
+++ lib/screens/profile/components/body.dart
@@ -8,36 +8,38 @@ class Body extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        ProfilePic(),
-        const SizedBox(height: 20),
-        ProfileMenu(
-          icon: "assets/icons/User Icon.svg",
-          text: "My Account",
-          press: () {},
-        ),
-        ProfileMenu(
-          icon: "assets/icons/Bell.svg",
-          text: "Notifications",
-          press: () {},
-        ),
-        ProfileMenu(
-          icon: "assets/icons/Settings.svg",
-          text: "Settings",
-          press: () {},
-        ),
-        ProfileMenu(
-          icon: "assets/icons/Question mark.svg",
-          text: "Help Center",
-          press: () {},
-        ),
-        ProfileMenu(
-          icon: "assets/icons/Log out.svg",
-          text: "Log Out",
-          press: () {},
-        ),
-      ],
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          ProfilePic(),
+          const SizedBox(height: 20),
+          ProfileMenu(
+            icon: "assets/icons/User Icon.svg",
+            text: "My Account",
+            press: () {},
+          ),
+          ProfileMenu(
+            icon: "assets/icons/Bell.svg",
+            text: "Notifications",
+            press: () {},
+          ),
+          ProfileMenu(
+            icon: "assets/icons/Settings.svg",
+            text: "Settings",
+            press: () {},
+          ),
+          ProfileMenu(
+            icon: "assets/icons/Question mark.svg",
+            text: "Help Center",
+            press: () {},
+          ),
+          ProfileMenu(
+            icon: "assets/icons/Log out.svg",
+            text: "Log Out",
+            press: () {},
+          ),
+        ],
+      ),
     );
   }
 }
